fd5d3e5d538e740416c738608eca1886ff4f7613
juannpmari <juannpmari@gmail.com>
2025-11-29 23:15:51 -0300

    Use asyncio.Future's to manage communication between async FastAPI and synchronous vLLM's .step()

--- data-plane/gateway/routing.py
+++ data-plane/gateway/routing.py
@@ -1,3 +1,5 @@
+from starlette.concurrency import run_in_threadpool
+
 # Load-aware and prefix-cache router
 
 # TODO: implement true load-aware routing (ie., based on queue length)
@@ -10,8 +12,6 @@ def route(model_name: str, lora_adapter: str):
 
 
 
-# API gateway (fastapi)
-
 from fastapi import FastAPI
 from inference.inference import Engine
 # from inference.kv_cache import KVCache
@@ -20,24 +20,27 @@ app = FastAPI()
 
 # kv_cache = KVCache()
 
-# Define all models and their configurations you want to load at startup
 MODEL_CONFIGS = {
     "mistral-7b": {"max_tokens": 4096},
     "llama-3-8b": {"max_tokens": 8192},
-    # Add more models here...
+    # Add more models
 }
 
 # The cache will hold the loaded Engine objects
 app.state.model_cache = {} 
+app.state.batching_loops = []
 
 # Use the startup event to load ALL necessary models ONCE
 @app.on_event("startup")
 async def startup_event():
-    print("Starting multi-model server loading...")
+    """
+    1. Load all models into their engines
+    2. Start the continous batching loop for each engine
+    """
+    print("Starting multi-model server")
     
     for model_name, config in MODEL_CONFIGS.items():
         print(f"Loading model: {model_name}...")
-        
         engine = Engine(
             model_name=model_name,
             args={
@@ -45,18 +48,35 @@ async def startup_event():
             }
         )
         app.state.model_cache[model_name] = engine
-    
     print("All configured models loaded successfully.")
 
+    # Start the continous batching loop for each engine
+    for _, engine in app.state.model_cache.items():
+        app.state.batching_loops.append(asyncio.create_task(engine.continous_batching_loop()))
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    """ Cancel the batching loop of each engine when the server shuts down"""
+    for loop in app.state.batching_loops:
+        loop.cancel()
+        print("Continuous batching loop cancelled.")
+
+
 @app.post("/generate")
-def generate(event: dict):
+async def generate(event: dict):
+    """
+    Generate a response for a given prompt using the specified model
+    """
     prompt = event['prompt']
     model_name = event['model']
-    max_tokens = event['max_tokens']
-    stream = event.get('stream', False)
+    # max_tokens = event['max_tokens']
+    # stream = event.get('stream', False)
     # lora_adapter = event['lora_adapter']
 
     engine = app.state.model_cache.get(model_name)
     if engine is None:
         raise HTTPException(status_code=404, detail=f"Model {model_name} not found")
-    return engine.generate(prompt, max_tokens=max_tokens)
+
+    result = await engine.add_request(prompt=prompt)
+
+    return result
--- data-plane/inference/engine.py
+++ data-plane/inference/engine.py
@@ -18,31 +18,50 @@ class Engine:
         ]
         args = parser.parse_args(cli_args_list)
         engine_args = EngineArgs.from_cli_args(args)
-        self.engine =  LLMEngine.from_engine_args(engine_args)
         self.sampling_params = SamplingParams(temperature=0.0)
+        
+        self.engine =  LLMEngine.from_engine_args(engine_args)
+        self.request_counter = 0
+        self.request_futures:DictÑ[str,asyncio.Future] = {}
 
-    def _generate_request_id(self):
-        return str(uuid.uuid4())
+    async def add_request(self, prompt: str, sampling_params: SamplingParams = {}):
+        request_id = str(self.request_counter)
+        self.request_counter += 1
+        
+        future = asyncio.Future()
+        self.request_futures[request_id] = future
 
-    def generate(self, prompt: str, **opts):
-        request_id = self._generate_request_id()
         self.engine.add_request(
             request_id,
             prompt,
             self.sampling_params
         )
 
-        for i in range(opts.get("max_tokens", 10)):
-            result = self.engine.step()
-            if result is not None:
-                return result
+        return await future
 
-    def generate_stream(self, prompt: str, **opts):
-        yield from self.engine.generate_stream(prompt, **opts)
+    async def continous_batching_loop(self):
+        print("Starting continous batching loop...")
+        while True:
+            if not self.engine.has_unfinished_requests(): #Nothing to process
+                await asyncio.sleep(0.01)
+                continue
+            
+            outputs_list: List[Any] = await asyncio.to_thread(self.engine.step) #generates one token for each request
 
-    def add_lora(self, lora_adapter: str):
-        self.engine.add_lora(lora_adapter)
+            for output in outputs_list:
+                request_id = output.request_id
+                future = self.request_futures.get(request_id)
+                
+                if future and not future.done():
+                    if output.finished: #Request reached EOS token or max_tokens so it's finished
+                        response = output.outputs[0].text
+                        future.set_result(response)
+                        del self.request_futures[request_id]
+                        # NOTE: For streaming, send partial results here
 
-    def batch_requests(self, prompts: list[str], **opts):
-        return self.engine.batch_requests(prompts, **opts)
+    # def generate_stream(self, prompt: str, **opts):
+    #     yield from self.engine.generate_stream(prompt, **opts)
+
+    # def add_lora(self, lora_adapter: str):
+    #     self.engine.add_lora(lora_adapter)
         
