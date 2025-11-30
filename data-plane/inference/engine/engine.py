95f2cde068adb57eba6b3638e8bdcac4f8286c19
juannpmari <juannpmari@gmail.com>
2025-11-30 11:56:49 -0300

    add lora support (engine side)

--- data-plane/inference/engine/engine.py
+++ data-plane/inference/engine/engine.py
@@ -1,13 +1,17 @@
 from vllm import EngineArgs, LLMEngine, SamplingParams
 from vllm.utils import FlexibleArgumentParser # A custom parser for vLLM
+from vllm.lora.request import LoRARequest
 from typing import Dict, List, Any
 import asyncio
+import requests
+
+
+SIDECAR_BASE_URL = "http://localhost:8001"
 
 class Engine:
-    def __init__(self, model_name: str, lora_adapter: str = None, args: dict = {}):
+    def __init__(self, model_name: str, args: dict = {}):
         self.model_name = model_name
         self.args = args
-        # self.lora_adapter = lora_adapter
         parser = FlexibleArgumentParser()
         parser = EngineArgs.add_cli_args(parser)
 
@@ -25,12 +29,29 @@ class Engine:
         self.request_counter = 0
         self.request_futures:Dict[str,asyncio.Future] = {}
 
-    async def add_request(self, prompt: str, sampling_params: SamplingParams = {}):
+    async def add_request(self, prompt: str, adapter_identifier:str = None, adapter_version:str = None, sampling_params: SamplingParams = {}):
         request_id = str(self.request_counter)
         self.request_counter += 1
         
         future = asyncio.Future()
         self.request_futures[request_id] = future
+
+        # TODO: offload this to a cache so that subsequent requests don't go through the latency of connecting to the sidecar (Cache-and-Wait Synchronization)
+        if adapter_identifier:
+            try:
+                response = asyncio.to_thread(
+                    requests.post,
+                    f"{SIDECAR_BASE_URL}/adapter/fetch/{adapter_identifier}",
+                    json={"version": adapter_version}
+                )
+                response.raise_for_status()
+                adapter = response.json()
+                adapter_path = adapter["local_path"]
+                
+                await self._add_lora(adapter_path, adapter_identifier, adapter_version)
+            except Exception as e:
+                future.set_exception(RuntimeError(f"Failed to load adapter {adapter_identifier} v{adapter_version}: {e}"))
+                return await future
 
         self.engine.add_request(
             request_id,
@@ -63,6 +84,15 @@ class Engine:
     # def generate_stream(self, prompt: str, **opts):
     #     yield from self.engine.generate_stream(prompt, **opts)
 
-    # def add_lora(self, lora_adapter: str):
-    #     self.engine.add_lora(lora_adapter)
+    
+    # TODO: complete this connection to add required adapters on demand 
+    async def _add_lora(self, adapter_path: str, adapter_identifier: str, adapter_version: str = "latest"):
+        adapter_path = "/path/to/lora/files" #TODO: get from sidecar
+        adapter_int_id = hash(adapter_identifier + adapter_version) & 0xFFFFFFFF
+        request = LoRARequest(
+            lora_name=f"{adapter_identifier}-{adapter_version}",
+            lora_int_id=adapter_int_id,
+            lora_path=adapter_path,
+        )
+        await self.engine.add_lora(request)
         
